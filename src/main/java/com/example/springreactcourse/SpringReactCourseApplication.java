5716998f053a2c3867bbf560d106a04e8edd097f
Philip Nedelev <philipnedelev@gmail.com>
2020-07-20 15:06:50 +0300

    first commit

---
+++ src/main/java/com/example/springreactcourse/SpringReactCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactCourseApplication.class, args);
+    }
+
+}
